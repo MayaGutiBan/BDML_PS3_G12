3852d3f8417de21463f1bdeb0380c4d707ee87b6
carlosmanpipe <84484424+carlosmanpipe@users.noreply.github.com>
2025-05-23 07:19:24 -0500

    Update PRE_PROSSES.R

--- scripts/PRE_PROSSES.R
+++ scripts/PRE_PROSSES.R
@@ -1,5 +1,5 @@
-# Cargar pacman (contiene la función p_load)
 library(pacman) 
+library(purrr)
 library(stringr)
 
 # Cargar las librerías listadas e instalarlas en caso de ser necesario
@@ -14,76 +14,144 @@ p_load(rio, ## read datasets
        spatialsample # validación cruzada espacial
 ) 
 
-### Pasar de texto a datos 
-limpiar_y_enriquecer <- function(df) {
-  df <- df %>%
-    # Limpieza del texto
-    mutate(description = str_to_lower(description)) %>%
-    mutate(description = stringi::stri_trans_general(description, "Latin-ASCII")) %>%
-    mutate(description = str_replace_all(description, "[^[:alnum:]]", " ")) %>%
-    mutate(description = str_trim(gsub("\\s+", " ", description))) %>%
-    
-    # Inferencia del tipo de propiedad
-    mutate(property_type_2 = ifelse(grepl("casa", description), "Casa", property_type)) %>%
-    mutate(property_type_2 = ifelse(grepl("apto|apartamento", description), "Apartamento", property_type_2)) %>%
-    
-    # Extracción de número de alcobas
-    mutate(
-      alcoba_raw = str_extract(description, "\\d+\\s*(alcobas?|cuartos?|habitaciones?)"),
-      alcobas = str_extract(alcoba_raw, "\\d+") %>% as.integer(),
-      bedrooms = if_else(is.na(bedrooms), alcobas, bedrooms)
-    ) %>%
-    
-    # Extracción de si tiene terraza
-    mutate(
-      tiene_terraza = if_else(str_detect(description, regex("terraza", ignore_case = TRUE)), 1, 0)
-    ) %>%
-    
-    # Extracción de número de baños
-    mutate(
-      baño_raw = str_extract(description, "\\d+\\s*(baños?|baño|banos|bano)"),
-      baños = str_extract(baño_raw, "\\d+") %>% as.integer(),
-      bathrooms = if_else(is.na(bathrooms), baños, bathrooms)
-    ) %>%
-    
-    # Extracción de área construida y asignación a surface
-    mutate(
-      area_matches = str_extract_all(description, pattern_context),
-      area_nums = map(area_matches, ~ {
-        nums <- str_extract_all(.x, "\\d{1,4}") %>% unlist() %>% as.integer()
-        nums[!is.na(nums) & nums >= 30 & nums <= 1000]
-      }),
-      area_construida = map_int(area_nums, ~ if(length(.x) > 0) max(.x) else NA_integer_),
-      surface_covered = if_else(is.na(surface_covered), area_construida, surface_covered),
-      surface_total = if_else(is.na(surface_total), area_construida, surface_total)
-    )%>%
-    
-    # Extracción de metros de terraza
-    mutate(
-      terraza_raw = str_extract(description, "(de\\s+)?terraza|patio|balcon\\s+\\d+\\s*(m2|mts|metros)?"),
-      m2_terraza = str_extract(terraza_raw, "\\d+") %>% as.integer()
-    ) %>%
-    
-    # Ajuste final de surface_total si aún está NA
-    mutate(
-      surface_total = if_else(
-        is.na(surface_total) & !is.na(surface_covered),
-        surface_covered + coalesce(m2_terraza, 0),
-        surface_total
-      )
-    ) %>%
-    
-    # Eliminación de columnas auxiliares
-    select(-alcoba_raw, -alcobas, -baño_raw, -baños, -area_construida, -terraza_raw, -m2_terraza)
-  
-  return(df)
-}
+library(keras)
+
+setwd("/Users/carlosmanjarres/Desktop/Big_data/uniandes-bdml-2025-10-ps-3")
+
+#### CARGAR BASE DE DATOS LIMPIA 
+
+db <- readRDS("train_clean_2.RDS")
+
+#### SELECCIÓNAR VARIABLES RELEVANTES 
+
+db <- db %>% select(property_type,dist_to_tm,num_restaurantes_700m,near_cai,localidad,
+                    tiene_terraza,surface_covered,price,bathrooms,bedrooms,price)
 
 
-train <- limpiar_y_enriquecer(train)
-test <- limpiar_y_enriquecer(test)
+#### SELECCIONAR LOCALIDADES SIMILARES A CHAPINERO 
+
+db <- db %>%
+  filter(localidad == "Localidad Chapinero" |
+           localidad == "Localidad Usaquén" |
+           localidad == "Localidad Engativá" |
+           localidad == "Localidad Suba" |
+           localidad == "Localidad Teusaquillo")
 
 
+val_set <- db %>% filter(localidad == "Localidad Chapinero")
+train <- db %>% filter(localidad != "Localidad Chapinero")
 
-p_load( visdat)
-vis_dat(train)
+### 1 . PREPROSSES DATA FOR KERAS ### 
+
+# 
+train <- train %>%
+  mutate(tiene_terraza = ifelse(is.na(tiene_terraza), 
+                                0,  
+                                tiene_terraza))
+train <- train %>% select(-localidad)
+
+dmy <- caret::dummyVars(
+  ~ .,
+  data = train,
+  sep = "_", # Separador para las variables dummy
+  drop = TRUE,
+  fullRank = TRUE # Evitar la multicolinealidad
+)
+
+train <- as.data.frame(predict(dmy, newdata = train))
+
+X_train <- train %>% select(-price)
+y_train <- train$price
+
+X_train <- as.matrix(train %>% select(-price))
+y_train <- as.matrix(train$price)
+
+val_set <- val_set %>% select(-localidad)
+
+dmy2 <- caret::dummyVars(
+  ~ .,
+  data = val_set,
+  sep = "_", # Separador para las variables dummy
+  drop = TRUE,
+  fullRank = TRUE # Evitar la multicolinealidad
+)
+
+val_set <- as.data.frame(predict(dmy2, newdata = val_set))
+
+X_test <- as.matrix(val_set %>% select(-price))
+y_test <- as.matrix(val_set$price)
+
+
+#### 2. Keras Model Definition Function ####
+
+model <- keras_model_sequential() %>%
+  layer_dense(units = 512, activation = "relu", input_shape = c(ncol(X_train))) %>%
+  layer_dropout(rate = 0.2) %>% # Adding dropout for regularization
+  layer_dense(units = 256, activation = "relu") %>%
+  layer_dropout(rate = 0.2) %>%
+  layer_dense(units = 256, activation = "relu") %>%
+  layer_dropout(rate = 0.2) %>%
+  layer_dense(units = 256, activation = "relu") %>%
+  layer_dropout(rate = 0.2) %>%
+  layer_dense(units = 1, activation = "relu", name = "capa_salida")
+
+
+### 3.  model compillation ###
+model %>% compile(
+  optimizer = optimizer_adam(learning_rate = 0.001),
+  loss = "mse",      # Mean Squared Error for regression
+  metrics = list("mae") # Mean Absolute Error
+)
+
+
+#### 4. train model 
+
+history <- model %>% fit(
+  X_train, y_train,
+  epochs = 100,
+  batch_size = 32,
+  validation_split = 0.2     # o usar x_val, y_val separados
+)
+
+plot(history)
+
+summary(model)
+
+#### Evaluar modelo con las observaciones de chapinero 
+
+model %>% evaluate(as.matrix(X_test), y_test)
+
+### kaggle submission 
+
+test <- read_rds("cleantest.RDS")
+
+test <- test %>%
+  mutate(tiene_terraza = ifelse(is.na(tiene_terraza), 
+                                0,  
+                                tiene_terraza))
+
+kaggle <- test%>% select( property_type,dist_to_tm,num_restaurantes_700m,near_cai,
+                          tiene_terraza,surface_covered,bathrooms,bedrooms)
+
+dmy3 <- caret::dummyVars(
+  ~ .,
+  data = kaggle,
+  sep = "_", # Separador para las variables dummy
+  drop = TRUE,
+  fullRank = TRUE # Evitar la multicolinealidad
+)
+
+kaggle <- as.data.frame(predict(dmy3, newdata = kaggle))
+
+prob_predictions <- model %>% 
+  predict(as.matrix(kaggle), batch_size = 32)
+
+submission_df <- data.frame(
+  property_id = test$property_id, 
+  price = as.vector(prob_predictions)     
+)
+
+any(is.na(submission_df))
+
+write.csv(submission_df, "submission_NNM_New.csv", row.names = FALSE)
+
